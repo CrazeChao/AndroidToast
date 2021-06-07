d52e7eb02dffdbded3bf1c7dc6ca142cd95b8249
nhlzc <li962834>
2021-06-07 20:06:42 +0800

    命令行提交

--- app/src/main/java/com/android/androidtoast/MainActivity.java
+++ app/src/main/java/com/android/androidtoast/MainActivity.java
@@ -14,10 +14,8 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
     }
-
-
-
     int count = 0;
+    int test = 0
     ToastCompat.IToast toast;
     public void showToast(View view){
         count++;
